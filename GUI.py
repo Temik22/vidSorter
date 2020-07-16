131785dcdfb2738b4aee6b61a1a783f9054b5c9f
Deadvitekchpool <vitek190499@gmail.com>
2020-07-16 05:06:39 +0400

    B

--- GUI.py
+++ GUI.py
@@ -10,27 +10,35 @@ class Example(Frame):
     def insertText(self, txt):
     	file = fd.askopenfilename(initialdir = "/")
     	f = open(file)
-    	s = f.read()
-    	txt.delete(1.0, END)
-    	txt.insert(END, s)
+    	s = f.read().split(' ')
+    	#txt.delete(1.0, END)
+    	for i in s:
+    		txt.insert(END, i)
     	f.close()
 
     def initUI(self):
         self.parent.title("Review")
         self.pack(fill=BOTH, expand=True)
         
-        frame1 = Frame(self)
-        frame1.pack(fill=BOTH, expand = True)         
-        
+        frame1 = Frame(self, width = 100)
+        frame1.pack(fill=Y, side = RIGHT)         
+        # frame1.grid(column=1, columnspan=5)
 
         btn1 = Button(frame1, text="Add", width=6)
-        btn1.pack(side=RIGHT, anchor = N, padx=5, pady=5) 
+        btn1.pack(side=TOP, anchor = N, padx=5, pady=5) 
+        
 
-        txt = Text(frame1)
+        btn2 = Button(frame1, text="Run", width=6)
+        btn2.pack(side=TOP, padx=5, pady=5)
+        
+        frame2 = Frame(self, width = 500)
+        frame2.pack(fill=BOTH, side = LEFT, expand = True)
+        # frame2.grid(column=2)
+
+        txt = Listbox(frame2)
         txt.pack(fill=BOTH, pady=5, padx=5, expand=True)
 
-        btn1.bind('<Button-1>', lambda event, t = txt: self.insertText(t)) 
-
+        btn1.bind('<Button-1>', lambda event, t = txt: self.insertText(t))
  
 root = Tk()
 root.geometry("600x400+300+300")
